2f8414c31809b493f6abc31a0d1ca23e6854510b
guy <guy3540@gmail.com>
2020-11-20 08:59:15 +0200

    Created environment to train A2C agent, and save its observations The model is currently saved only once (at the end of the training process)

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello")
+import gym
+from HCF_functions import get_paddle_position, get_max_tunnel_depth, get_ball_position
+from HCF_gym_wrapper import HCFgymWrapper
+from stable_baselines.common import make_vec_env
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import A2C
+
+
+NUM_TIMESTEPS = 50000
+NUM_OF_PROCESSES = 16
+
+# Parallel environments
+env = make_vec_env('Breakout-v4', n_envs=NUM_OF_PROCESSES, wrapper_class=HCFgymWrapper)
+
+model = A2C(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=NUM_TIMESTEPS)
+model.save("a2c_Breakout-v4")
+model.env.close()
+
+del model  # remove to demonstrate saving and loading
+
+model = A2C.load("a2c_Breakout-v4")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
